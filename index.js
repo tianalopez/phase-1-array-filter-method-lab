d45200e94d7c1dc8f8a1e208bbc2143329b65fcd
Tiana Lopez <tianadavin@gmail.com>
2023-09-16 15:57:56 -0700

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+  const result = array.filter((name) => name.toLowerCase() === string.toLowerCase())
+  return result
+}
+
+function fuzzyMatch(array, string) {
+  const result = array.filter((name) =>
+    name.substr(0,string.length) === string
+  )
+  return result
+}
+
+function matchName(array, string) {
+  const result = array.filter((obj) =>
+  string === obj.name
+  )
+  return result
+}
